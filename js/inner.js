fc8b5d8a8cca8cc0056eee78064418c8a44fd1a6
Petia Naumova <petianaumova@gmail.com>
2015-03-03 18:04:49 +0200

    Ne znam kakvo napravihme

--- js/inner.js
+++ js/inner.js
@@ -18,6 +18,7 @@ function Applicant(id, name, email, experience){
 	this.email = email;
 	this.experience = experience;
     this.ChoosenJobs = [];
+    // ApplyForJob method - this is a method I use to mark job offers the applicant has applied to.
 	this.ApplyForJob = function(JobOfferInstance){
         this.ChoosenJobs.push(JobOfferInstance.id);
 
@@ -73,57 +74,61 @@ window.addEventListener("load", function(){
             opt.innerHTML = ApplicantsContainer[i].name;
             select.appendChild(opt);
         }
-        //select.addEventListener("change", function(){
-        //    alert("Hello");
-       // });
+        select.addEventListener("change", function(){
+            ConstructTableOffers();
+        });
     };
     selectApplicant();
 };
 
 function ConstructTableOffers(){
-        var JobOfferListTable = document.getElementById("JobOffersList");
-        while(JobOfferListTable.rows.length!=1){
+    var JobOfferListTable = document.getElementById("JobOffersList");
+    while(JobOfferListTable.rows.length!=1){
         JobOfferListTable.deleteRow(1);
     };
     var JobOffersList = document.getElementById("JobOffersList");
-    
+    var select = document.getElementById("applicantDropDown");
+    var ApplicantID = parseInt(select.value, 10);
+    // first, we must find an applicant from Applicants array by giving it an ID
+    var Applicant;
+    for(var i=0; i< ApplicantsContainer.length;i++){
+        if(ApplicantsContainer[i].id === ApplicantID){
+            Applicant = ApplicantsContainer[i];
+            break;
+        }
+    };
+
     for(var i=0;i<JobOffersContainer.length;i++){
-        var newRow = JobOffersList.insertRow(-1);
-        var TitleCell = newRow.insertCell(-1);
-        TitleCell.innerHTML = JobOffersContainer[i].title;
-        var descriptionCell = newRow.insertCell(-1);
-        descriptionCell.innerHTML = JobOffersContainer[i].description;
-        var requirementsCell = newRow.insertCell(-1);
-        requirementsCell.innerHTML = JobOffersContainer[i].requirements;
-        var addressCell = newRow.insertCell(-1);
-        addressCell.innerHTML = JobOffersContainer[i].address;
-        var buttonCell = newRow.insertCell(-1);
-        var button = document.createElement("button");
-        button.setAttribute("class", "btn btn-info");
-        button.textContent = "Apply";
-        buttonCell.appendChild(button);
-        button.id = JobOffersContainer[i].id;
-        button.addEventListener("click", function(){
-            var select = document.getElementById("applicantDropDown");
-            var ApplicantID = parseInt(select.value, 10);
-            var JobOfferID = parseInt(this.id, 10);
-            // first, we must find an applicant from Applicants array by giving it an ID
-            var Applicant;
-            for(var i=0; i< ApplicantsContainer.length;i++){
-                if(ApplicantsContainer[i].id === ApplicantID){
-                    Applicant = ApplicantsContainer[i];
-                    break;
+        if(Applicant.ChoosenJobs.indexOf(JobOffersContainer[i].id) === -1){
+            var newRow = JobOffersList.insertRow(-1);
+            var TitleCell = newRow.insertCell(-1);
+            TitleCell.innerHTML = JobOffersContainer[i].title;
+            var descriptionCell = newRow.insertCell(-1);
+            descriptionCell.innerHTML = JobOffersContainer[i].description;
+            var requirementsCell = newRow.insertCell(-1);
+            requirementsCell.innerHTML = JobOffersContainer[i].requirements;
+            var addressCell = newRow.insertCell(-1);
+            addressCell.innerHTML = JobOffersContainer[i].address;
+            var buttonCell = newRow.insertCell(-1);
+            var button = document.createElement("button");
+            button.setAttribute("class", "btn btn-info");
+            button.textContent = "Apply";
+            buttonCell.appendChild(button);
+            button.id = JobOffersContainer[i].id;
+            button.addEventListener("click", function(){
+                var JobOfferID = parseInt(this.id, 10);
+                var JobOffer;
+                for(var i=0; i<JobOffersContainer.length;i++){
+                    if(JobOffersContainer[i].id === JobOfferID){
+                        JobOffer = JobOffersContainer[i];
+                        break;
+                    }
                 }
-            }
-            var JobOffer;
-            for(var i=0; i<JobOffersContainer.length;i++){
-                if(JobOffersContainer[i].id === JobOfferID){
-                    JobOffer = JobOffersContainer[i];
-                    break;
-                }
-            }
-            Applicant.ApplyForJob(JobOffer);
-        });
+                Applicant.ApplyForJob(JobOffer);
+
+                ConstructTableOffers();
+            });
+        }
     };
 };
 ConstructTableApplicants();
